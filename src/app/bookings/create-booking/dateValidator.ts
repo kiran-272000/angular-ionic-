885df7c15c4de50f1bcbd0df213220769518d12d
kiran-272000 <vellingirikiran@gmail.com>
2022-08-24 21:42:22 +0530

    initial

--- src/app/bookings/create-booking/dateValidator.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl } from "@angular/forms";
-
-export function dateValidator(control : AbstractControl){
-    const startDate = new Date(this.form.get('from').value);
-    const endDate = new Date(this.form.get('to').value);
-
-    return endDate > startDate
-  }
