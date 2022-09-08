ae76ffff3aba31dfacf7136c7b4d7b3c498664bf
kiran <vellingirikiran@gmail.com>
2022-09-08 09:11:05 +0530

    operators-learning

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,37 +1,78 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { interval, of, Subscription } from "rxjs";
+import {
+  delay,
+  filter,
+  take,
+  tap,
+  mergeMap,
+  concatMap,
+  switchMap,
+  exhaustMap,
+} from "rxjs/operators";
 
-import { Place } from '../../place.model';
-import { PlacesService } from '../../places.service';
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
-  selector: 'app-offer-bookings',
-  templateUrl: './offer-bookings.page.html',
-  styleUrls: ['./offer-bookings.page.scss']
+  selector: "app-offer-bookings",
+  templateUrl: "./offer-bookings.page.html",
+  styleUrls: ["./offer-bookings.page.scss"],
 })
 export class OfferBookingsPage implements OnInit, OnDestroy {
   place: Place;
-  private placeSub : Subscription
+  private placeSub: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('placeId')) {
-        this.navCtrl.navigateBack('/places/tabs/offers');
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
         return;
       }
-      this.placesService.getPlace(paramMap.get('placeId')).subscribe(place=>{this.place=place});
+      this.placesService
+        .getPlace(paramMap.get("placeId"))
+        .subscribe((place) => {
+          this.place = place;
+        });
     });
+
+    // let postIds = interval(1).pipe(
+    //   filter((val) => val > 0),
+    //   take(10)
+    // );
+
+    // postIds
+    //   .pipe(
+    //     // delay(1000),
+    //     // tap((t) => {
+    //     //   console.log(t + "hi");
+    //     // }),
+    //     // filter((val) => val % 2 === 1),
+    //     // take(5),
+    //     // delay(500)
+    //     switchMap((id) => {
+    //       return this.http.get(
+    //         `http://jsonplaceholder.typicode.com/posts/${id}`
+    //       );
+    //     })
+    //   )
+    //   .subscribe((res) => {
+    //     // console.log(res);
+    //   });
   }
   ngOnDestroy() {
     if (this.placeSub) {
-      this.placeSub.unsubscribe();}
+      this.placeSub.unsubscribe();
+    }
   }
 }
